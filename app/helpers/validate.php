35e1b8b34f3bbbf38151c46431806cf864690b0b
DanielTibaes505 <daniel.tibaes@outlook.com>
2023-06-03 15:24:55 -0300

    Sistema de Login - Falta ainda confirmação pass e senhas com caracteres diferentes

--- app/helpers/validate.php
+++ app/helpers/validate.php
@@ -9,13 +9,13 @@
     $term = $_POST['checkboxTerm'];
 
     if(empty($name) || strlen($name) < 3) {
-        $emptyEmail =  '<a href = "../../public/pages/registration.php" class="btnSubmit">CORRIJA SEU NOME NO FORMULÁRIO</a>';
+        $emptyEmail =  '<a href = "../../public/pages/registration.html" class="btnSubmit">CORRIJA SEU NOME NO FORMULÁRIO</a>';
     }elseif(empty($email)  || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $emptyEmail =  '<a href = "../../public/pages/registration.php" class="btnSubmit">CORRIJA SEU E-MAIL NO FORMULÁRIO</a>';
+        $emptyEmail =  '<a href = "../../public/pages/registration.html" class="btnSubmit">CORRIJA SEU E-MAIL NO FORMULÁRIO</a>';
     }elseif(empty($country)) {
-        $emptyCountry =  '<a href = "../../public/pages/registration.php" class="btnSubmit">CORRIJA SEU PAIS NO FORMULÁRIO</a>'; 
+        $emptyCountry =  '<a href = "../../public/pages/registration.html" class="btnSubmit">CORRIJA SEU PAIS NO FORMULÁRIO</a>'; 
     }elseif(empty($term)){
-        $emptyTerm =  '<a href = "../../public/pages/registration.php" class="btnSubmit">ACEITE OS TERMOS DE CADASTRO</a>';
+        $emptyTerm =  '<a href = "../../public/pages/registration.html" class="btnSubmit">ACEITE OS TERMOS DE CADASTRO</a>';
     }else{
         if( $_SERVER['REQUEST_METHOD']=='POST' )
         {
@@ -41,6 +41,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="../assets/images/triangle.png" type="image/x-icon">
     <link rel="stylesheet" href="../../public/assets/css/global.css">
     <link rel="stylesheet" href="../../public/assets/css/oppss.css">
     <title>OPPS...</title>
